8841807b3ed858ec1a8de14e548f554bf4c65656
John Doak <jdoak@ElephantInTheRoom.local>
2024-04-08 08:24:43 -0700

    Updating storage definitions and adding support for List() and Search()

--- workflow/storage/storage.go
+++ workflow/storage/storage.go
@@ -4,6 +4,8 @@ package storage
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/element-of-surprise/workstream/workflow"
 	"github.com/element-of-surprise/workstream/workflow/storage/internal/private"
@@ -11,22 +13,61 @@ import (
 	"github.com/google/uuid"
 )
 
-// SearchFilter is a filter for searching Plans.
-type SearchFilter struct{}
+// Filters is a filter for searching Plans.
+type Filters struct{
+	// ByIDs is a list of Plan IDs to search by.
+	ByIDs []uuid.UUID
+	// ByGroupIDs is a list of Group IDs to search by.
+	ByGroupIDs []uuid.UUID
+	// ByStatus is a list of Plan states to search by.
+	ByStatus []workflow.Status
+}
+
+// Validate validates the search filter.
+func (f Filters) Validate() error {
+	if len(f.ByIDs) + len(f.ByGroupIDs) + len(f.ByStatus) == 0 {
+		return fmt.Errorf("at least one search filter must be provided")
+	}
+	return nil
+}
+
+// Stream represents an entry in a stream of data.
+type Stream[T any] struct {
+	// Result is a result in the stream.
+	Result T
+	// Err is an error in the stream.
+	Err    error
+}
 
 // ReadWriter is a storage reader and writer for Plan data. An implementation of ReadWriter must ensure
 // atomic writes for all data.
 type ReadWriter interface {
 	PlanReader
 
-	// CreatePlan creates a new Plan in storage. This fails if the Plan ID already exists.
-	CreatePlan(ctx context.Context, plan *workflow.Plan) error
+	// Create creates a new Plan in storage. This fails if the Plan ID already exists.
+	Create(ctx context.Context, plan *workflow.Plan) error
 
-	// PlanWriter returns a PlanWriter for the given Plan ID.
-	PlanWriter(context.Context, uuid.UUID) (PlanWriter, error)
+	// Writer returns a PlanWriter for the given Plan ID.
+	Writer(context.Context, uuid.UUID) (PlanWriter, error)
 
 	// Close closes the storage.
 	Close(ctx context.Context) error
+}
+
+// ListResult is a result from a List operation.
+type ListResult struct {
+	// ID is the Plan ID.
+	ID uuid.UUID
+	// GroupID is the Group ID.
+	GroupID uuid.UUID
+	// Name is the Plan name.
+	Name string
+	// Descr is the Plan description.
+	Descr string
+	// SubmitTime is the Plan submit time.
+	SubmitTime time.Time
+	// State is the Plan state.
+	State *workflow.State
 }
 
 // PlanReader allows for reading Plan data from storage.
@@ -36,10 +77,10 @@ type PlanReader interface {
 	// Read returns a Plan from the storage.
 	Read(ctx context.Context, id uuid.UUID) (*workflow.Plan, error)
 	// Search returns a list of Plan IDs that match the filter.
-	Search(ctx context.Context, filter SearchFilter) ([]uuid.UUID, error)
+	Search(ctx context.Context, filters Filters) (chan Stream[ListResult], error)
 	// ListPlans returns a list of all Plan IDs in the storage. This should
 	// return with most recent submiited first.
-	List(ctx context.Context, limit int) ([]uuid.UUID, error)
+	List(ctx context.Context, limit int) (chan Stream[ListResult], error)
 
 	private.Storage
 }
