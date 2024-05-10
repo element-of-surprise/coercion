4730eb003e8b9f9535e6322edc33014254ebc414
John Doak <jdoak@ElephantInTheRoom.local>
2024-05-10 12:51:18 -0700

    Addings all the embeeded reporters

--- workflow/utils/html/internal/embedded/embedded.go
+++ workflow/utils/html/internal/embedded/embedded.go
@@ -10,9 +10,23 @@ var FS embed.FS
 // Those are handled in embedded_[os]_[arch].go files.
 
 // Note: Could be improved with UPX compression. https://github.com/upx/upx/
-//go:generate go build -ldflags "-s -w" -o reporter/reporter reporter/reporter.go
+//go:generate env GOOS=linux GOARCH=arm64 go build -ldflags "-s -w" -o reporter/reporter_linux_arm64 reporter/reporter.go
+//go:generate env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o reporter/reporter_linux_amd64 reporter/reporter.go
+//go:generate env GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w" -o reporter/reporter_darwin_arm64 reporter/reporter.go
+//go:generate env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o reporter/reporter_darwin_amd64 reporter/reporter.go
+//go:generate env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o reporter/reporter_windows_amd64.exe reporter/reporter.go
 
-//go:embed reporter/reporter
+//go:embed reporter/reporter_linux_arm64
+var ReporterLinuxArm64 []byte
 
-// Reporter provides a binary file that will read the reports from a sub-directory.
-var Reporter []byte
+//go:embed reporter/reporter_linux_amd64
+var ReporterLinuxAmd64 []byte
+
+//go:embed reporter/reporter_darwin_arm64
+var ReporterDarwinArm64 []byte
+
+//go:embed reporter/reporter_darwin_amd64
+var ReporterDarwinAmd64 []byte
+
+//go:embed reporter/reporter_windows_amd64.exe
+var ReporterWindowsAmd64 []byte
