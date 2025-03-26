72b1a6e67d4798edd3342550abdabaae5899503e
Shoshana <shmalfat@microsoft.com>
2025-03-26 09:15:59 -0700

    adding gauge

--- internal/execute/metrics/metrics.go
+++ internal/execute/metrics/metrics.go
@@ -19,6 +19,7 @@ const (
 
 var (
 	// currently running guage for started/running
+	runningCount metric.Int64UpDownCounter
 	// rename from workflowEventCount to sometheing like objectStateTransition? objectStateChange? objectStateCompletion? just objectState or
 	// objectStatus? executionState?
 	// is this confusing if something is retried? I guess that would only show up in attempts
@@ -32,11 +33,37 @@ func metricName(name string) string {
 // Init initializes the readers metrics. This should only be called by the tattler constructor or tests.
 func Init(meter api.Meter) error {
 	var err error
-	executionStatusCount, err = meter.Int64Counter(metricName("execution_status_total"), api.WithDescription("total number of watch events handled by tattler"))
+	runningCount, err = meter.Int64UpDownCounter(metricName("running_total"), api.WithDescription("total number of coercion workflow objects currently running"))
+	if err != nil {
+		return err
+	}
+	executionStatusCount, err = meter.Int64Counter(metricName("execution_status_total"), api.WithDescription("total number of coercion workflow objects executed"))
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+// StartRunning increases the runningCount metric.
+func StartRunning(ctx context.Context, t workflow.ObjectType, s workflow.Status) {
+	opt := api.WithAttributes(
+		// added, modified, deleted, bookmark, error
+		attribute.Key(objectTypeLabel).String(t.String()),
+	)
+	if runningCount != nil {
+		runningCount.Add(ctx, 1, opt)
+	}
+}
+
+// StopRunning decreases the runningCount metric.
+func StopRunning(ctx context.Context, t workflow.ObjectType, s workflow.Status) {
+	opt := api.WithAttributes(
+		// added, modified, deleted, bookmark, error
+		attribute.Key(objectTypeLabel).String(t.String()),
+	)
+	if runningCount != nil {
+		runningCount.Add(ctx, -1, opt)
+	}
 }
 
 // ExecutionStatus increases the watchEventCount metric
